8ea1057b3448c2e4cb67a47f10bf50aca666f5f1
Maksym Bekuzarov <maksym.bekuzarov@nure.ua>
2017-11-20 00:39:11 +0200

    Initial commit

---
+++ js/combinations.js
@@ -0,0 +1,46 @@
+/**
+ * Created by maxbe on 19.11.2017.
+ */
+function Combinations() {
+    this._combs = {
+        "water fire": "steam",
+        "dirt fire": "lava"
+    };
+    this.getCombo = function (first, second) {
+        return this._combs[first + " " + second] || this._combs[first + " " + second];
+    };
+}
+// Combinations.prototype.getCombo = function (first, second) {
+//     return this._combs[first + "" + second] || this._combs[first + "" + second];
+// };
+
+var comb = new Combinations();
+var chosen = new Chosen();
+
+function Chosen() {
+    this._chosen = [];
+    this.isFull = function () {
+        return (this.first() && this.second()) == true;
+    };
+    this.add = function (el) {
+        if (this.isFull()) {
+            this._chosen[0] = this._chosen[1];
+        }
+        if(!this._chosen[0])
+            this._chosen[0] = el;
+        else
+            this._chosen[1] = el;
+    };
+    this.remove = function (el) {
+        if (this._chosen[0] == el)
+            this._chosen.shift();
+        else if (this._chosen[1] == el)
+            this._chosen.pop();
+    };
+    this.first = function () {
+        return this._chosen[0];
+    };
+    this.second = function () {
+        return this._chosen[1];
+    }
+}
---
+++ js/engine.js
@@ -0,0 +1,19 @@
+/**
+ * Created by maxbe on 19.11.2017.
+ */
+
+function checkCombo() {
+    var x = comb.getCombo(chosen.first(), chosen.second());
+    if (x !== undefined) {
+        if (!$('#book > .element[name=' + x + ']').length) {
+            alert('Element ' + x + ' already exists.');
+        } else {
+            $('#book').append(jQuery('<div />', {
+                name: x,
+                class: 'element',
+                text: x
+            }).draggable(bookElementSettings).disableSelection());
+            alert("You've created " + x);
+        }
+    }
+}
